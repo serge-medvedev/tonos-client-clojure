c75c5398ea83d84f5a76d5a9768053f8d5b9382c
Serge Medvedev <thismoment.main@gmail.com>
2020-10-31 23:23:14 +0200

    test(client): basic tests are introduced

---
+++ test/tonos/client/client_test.clj
@@ -0,0 +1,37 @@
+(ns tonos.client.client-test
+  (:require
+    [clojure.test :refer :all]
+    [tonos.client.test-tools :as tt]
+    [tonos.client.client :as client]
+    [tonos.client.core :as core]))
+
+
+(def ^:dynamic *context* nil)
+
+(defn context-fixture
+  [f]
+  (binding [*context* (core/create-context tt/dev-net-config)]
+    (f)
+    (core/destroy-context *context*)))
+
+(use-fixtures :each context-fixture)
+
+(deftest version-test
+  (testing "getting client version"
+    (is (-> (client/version *context*)
+            doall
+            last
+            :params-json
+            :version
+            (= "1.0.0")))))
+
+(deftest api-reference-test
+  (testing "getting api reference"
+    (is (-> (client/get-api-reference *context*)
+            doall
+            last
+            :params-json
+            :api
+            nil?
+            false?))))
+
