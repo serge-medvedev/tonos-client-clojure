2a95ea2ef0319e44740f55b99903df7ff8c276ed
Serge Medvedev <thismoment.main@gmail.com>
2020-11-03 15:59:40 +0200

    test(utils): basic tests are introduced

---
+++ test/tonos/client/utils_test.clj
@@ -0,0 +1,32 @@
+(ns tonos.client.utils-test
+  (:require
+    [clojure.test :refer :all]
+    [tonos.client.test-tools :as tt]
+    [tonos.client.utils :as utils]
+    [tonos.client.core :as core]))
+
+
+(def ^:dynamic *context* nil)
+
+(defn- tf
+  [f]
+  (binding [*context* (core/create-context tt/main-net-config)]
+    (f)
+    (core/destroy-context *context*)))
+
+(use-fixtures :once tf)
+
+(deftest convert-address-test
+  (testing "converting the address"
+    (let [params {:address "0:b453e53ae4ae0d8104592c1127298aecb637bb70a0bcd56322cf7731a66ce1d2"
+                  :output_format {:type "Base64"
+                                  :url false
+                                  :test false
+                                  :bounce true}}]
+      (is (-> (utils/convert-address *context* params)
+              doall
+              last
+              :params-json
+              :address
+              (= "EQC0U+U65K4NgQRZLBEnKYrstje7cKC81WMiz3cxpmzh0sJJ"))))))
+
