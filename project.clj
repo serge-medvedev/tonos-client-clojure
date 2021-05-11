0721dff12871dc5bee5ae655b7a39fbffc802459
Serge Medvedev <thismoment.main@gmail.com>
2021-05-11 16:49:25 +0300

    chore: version bumped up

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tonos-client "1.13.0-SNAPSHOT"
+(defproject tonos-client "1.14.1-SNAPSHOT"
   :description "Clojure bindings to TON OS SDK's Core Client Library"
   :url "https://github.com/serge-medvedev/tonos-client-clojure"
   :license {:name "Eclipse Public License"
