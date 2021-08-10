c989abf6d17955eb2901b73e6bf973e8652e40ab
Serge Medvedev <thismoment.main@gmail.com>
2021-08-10 15:56:00 +0300

    chore: version bumped up

--- test/tonos/client/client_test.clj
+++ test/tonos/client/client_test.clj
@@ -20,7 +20,7 @@
   (testing "getting client version"
     (is (-> (client/version! *context*)
             :version
-            (= "1.20.0")))))
+            (= "1.20.1")))))
 
 (deftest api-reference-test
   (testing "getting api reference"
