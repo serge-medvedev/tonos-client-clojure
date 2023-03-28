c86f530c2c9053cf143aeb5dc6a3520ce04f94fc
Serge Medvedev <thismoment.main@gmail.com>
2023-03-28 20:59:36 +0300

    chore: auto-generated code updated for reference

--- target/generated/tonos/client/processing.clj
+++ target/generated/tonos/client/processing.clj
@@ -2,6 +2,51 @@
 (ns tonos.client.processing
   (:require [tonos.client.core :refer [request]]))
 
+(def monitor-messages
+  (partial request "processing.monitor_messages"))
+(defn monitor-messages!
+  [& args]
+  (-> (apply monitor-messages args)
+      doall
+      last
+      :params-json))
+
+(def get-monitor-info
+  (partial request "processing.get_monitor_info"))
+(defn get-monitor-info!
+  [& args]
+  (-> (apply get-monitor-info args)
+      doall
+      last
+      :params-json))
+
+(def fetch-next-monitor-results
+  (partial request "processing.fetch_next_monitor_results"))
+(defn fetch-next-monitor-results!
+  [& args]
+  (-> (apply fetch-next-monitor-results args)
+      doall
+      last
+      :params-json))
+
+(def cancel-monitor
+  (partial request "processing.cancel_monitor"))
+(defn cancel-monitor!
+  [& args]
+  (-> (apply cancel-monitor args)
+      doall
+      last
+      :params-json))
+
+(def send-messages
+  (partial request "processing.send_messages"))
+(defn send-messages!
+  [& args]
+  (-> (apply send-messages args)
+      doall
+      last
+      :params-json))
+
 (def send-message
   (partial request "processing.send_message"))
 
